997dde1a1b946d4914095369939e3b474af99feb
dev1ans <dev1ans@mail.ru>
2023-05-10 12:25:31 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int ticket =  100;
+
+        int mile = ticket / 20 ;
+
+
+
+
+        System.out.println("Бонусные мили: " +mile);
+        }
+
+
+
+    }
+
