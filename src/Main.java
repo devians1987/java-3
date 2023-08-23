e2d8fc50b6380548bc25ffc24b6493a791424ea0
dev1ans <dev1ans@mail.ru>
2023-08-23 13:44:04 +0500

    fir

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
 
 public class Main {
     public static void main(String[] args) {
-        int ticket =  100;
+        int ticket = 100800;
 
-        int mile = ticket / 20 ;
-
-
+        int mile = ticket / 20;
 
 
-        System.out.println("Бонусные мили: " +mile);
-        }
-
-
-
+        System.out.println("Бонусные мили: " + mile);
     }
+
+
+}
 
